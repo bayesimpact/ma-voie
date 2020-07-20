b6dbfae02ec7d357f0c7749520eba2cb9e2d23f8
Marie Laure Endale Ahanda <marielaure@bayesimpact.org>
2020-07-20 14:16:28 +0200

    [Landing] Add content for terms and conditions. Fix #8

--- src/components/pages/terms.tsx
+++ src/components/pages/terms.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
+import {useTranslation} from 'react-i18next'
 
-const TermsPage = (): React.ReactElement => {
-  return <div>
-    Terms and Conditions
+import {STATIC_NAMESPACE} from 'store/i18n'
+
+
+// This is a top level page and should be nested only with caution.
+// TOP LEVEL PAGE
+const TermsPage = (props: {style?: React.CSSProperties}): React.ReactElement => {
+  const {t} = useTranslation()
+  const [translate] = useTranslation(STATIC_NAMESPACE)
+  const {style = {padding: '40px 30px'}} = props
+  const email = 'contact@mavoie.org'
+  return <div style={style}>
+    <ReactMarkdown source={translate('termsOfService', {
+      canonicalUrl: t('canonicalUrl'),
+      host: 'gandi – 63-65 Boulevard Masséna 75013 Paris 01 70 37 76 61',
+      owner: 'Chance / MaVoie.org',
+      productName: t('productName').toLocaleUpperCase(),
+      publisher: `MaVoie.org – ${email} Le responsable publication ` +
+      'est une personne physique ou une personne morale.',
+      webmaster: 'John Métois – contact@mavoie.org',
+    })} />
   </div>
 }
 
