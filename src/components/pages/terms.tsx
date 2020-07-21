8b0a8223a747ef1b52ac37e73dbd6fd2623b0742
Pascal Corpet <pascal@bayesimpact.org>
2020-07-21 09:43:16 +0200

    [Terms] Style the terms page.
    Mocks https://zpl.io/V04lZZx
    Fix #13
    PATH=/cgu

--- src/components/pages/terms.tsx
+++ src/components/pages/terms.tsx
@@ -1,28 +1,135 @@
 import React from 'react'
-import ReactMarkdown from 'react-markdown'
 import {useTranslation} from 'react-i18next'
+import ReactMarkdown from 'react-markdown'
+import {Link} from 'react-router-dom'
 
 import {STATIC_NAMESPACE} from 'store/i18n'
+import {getPath} from 'store/url'
+
+import Footer from 'components/footer'
+import logoImage from 'images/logo.svg'
+
+
+const h2Style: React.CSSProperties = {
+  color: colors.DARK_FOREST_GREEN,
+  fontFamily: 'ProximaSoft',
+  fontSize: 32,
+  lineHeight: 1.25,
+  marginBottom: 20,
+}
+
+
+interface HeadingProps {
+  children: React.ReactNode
+  level: number
+}
+
+
+const CustomHeading = (props: HeadingProps): React.ReactElement => {
+  const {children, level} = props
+  if (level === 2) {
+    return <h2 style={h2Style}>{children}</h2>
+  }
+  return React.createElement(`h${level}`, {}, children)
+}
+
+
+const strongStyle: React.CSSProperties = {
+  color: colors.REDDISH_ORANGE,
+}
+
+
+const CustomStrong = ({children}: {children: React.ReactNode}): React.ReactElement => {
+  return <strong style={strongStyle}>{children}</strong>
+}
+
+
+const linkStyle: React.CSSProperties = {
+  color: colors.NEON_BLUE,
+}
+
+
+const CustomLink = <T extends Record<string, unknown>>(props: T): React.ReactElement => {
+  return <a {...props} rel="noopener noreferer" blank="_target" style={linkStyle} />
+}
+
+
+const titleSectionStyle: React.CSSProperties = {
+  backgroundColor: colors.TURQUOISE_BLUE,
+  color: '#fff',
+  overflow: 'hidden',
+  padding: '150px 20px 190px',
+  position: 'relative',
+}
+const titleStyle: React.CSSProperties = {
+  fontFamily: 'ProximaSoft',
+  fontSize: 47,
+  fontWeight: 'bold',
+  margin: '0 auto',
+  maxWidth: 700,
+  textTransform: 'uppercase',
+}
+const logoStyle: React.CSSProperties = {
+  left: 30,
+  margin: 0,
+  position: 'absolute',
+  top: 30,
+}
+const contentStyle: React.CSSProperties = {
+  color: colors.BATTLESHIP_GREY,
+  fontSize: 18,
+  lineHeight: 1.44,
+  margin: 'auto',
+  maxWidth: 700,
+}
+const whiteTriangleStyle: React.CSSProperties = {
+  borderBottom: 'solid #fff 10vw',
+  borderLeft: 'solid transparent 100vw',
+  bottom: 0,
+  left: 0,
+  position: 'absolute',
+  zIndex: 1,
+}
+
+
+const renderers = {
+  heading: CustomHeading,
+  link: CustomLink,
+  strong: CustomStrong,
+}
 
 
 // This is a top level page and should be nested only with caution.
 // TOP LEVEL PAGE
-const TermsPage = (props: {style?: React.CSSProperties}): React.ReactElement => {
+const TermsPage = (): React.ReactElement => {
   const {t} = useTranslation()
   const [translate] = useTranslation(STATIC_NAMESPACE)
-  const {style = {padding: '40px 30px'}} = props
   const email = 'contact@mavoie.org'
-  return <div style={style}>
-    <ReactMarkdown source={translate('termsOfService', {
-      canonicalUrl: t('canonicalUrl'),
-      host: 'gandi – 63-65 Boulevard Masséna 75013 Paris 01 70 37 76 61',
-      owner: 'Chance / MaVoie.org',
-      productName: t('productName').toLocaleUpperCase(),
-      publisher: `MaVoie.org – ${email} Le responsable publication ` +
-      'est une personne physique ou une personne morale.',
-      webmaster: 'John Métois – contact@mavoie.org',
-    })} />
-  </div>
+  return <React.Fragment>
+    <section style={titleSectionStyle}>
+      <Link style={logoStyle} to={getPath('SPLASH', t)}>
+        <img src={logoImage} alt={t('productName')} />
+      </Link>
+      <h1 style={titleStyle}>
+        {t('Mentions légales')}
+      </h1>
+      <div style={whiteTriangleStyle} />
+    </section>
+    <div style={{padding: '55px 20px 100px'}}>
+      <div style={contentStyle}>
+        <ReactMarkdown source={translate('termsOfService', {
+          canonicalUrl: t('canonicalUrl'),
+          host: 'gandi – 63-65 Boulevard Masséna 75013 Paris 01 70 37 76 61',
+          owner: 'Chance / MaVoie.org',
+          productName: t('productName').toLocaleUpperCase(),
+          publisher: `MaVoie.org – ${email} Le responsable publication ` +
+          'est une personne physique ou une personne morale.',
+          webmaster: 'John Métois – contact@mavoie.org',
+        })} renderers={renderers} />
+      </div>
+    </div>
+    <Footer />
+  </React.Fragment>
 }
 
 
