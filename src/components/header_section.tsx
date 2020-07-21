4ea59da460b5862d84c12facb4da8f738f8c1747
Cyrille Corpet <cyrille@bayesimpact.org>
2020-07-21 14:50:34 +0200

    [Mobile] Mobile-proof the header section of splash screen. (#69)
    * [Mobile] Mobile-proof the header section of splash screen.

--- src/components/header_section.tsx
+++ src/components/header_section.tsx
@@ -7,13 +7,14 @@ import orange13ArrowsImage from 'images/arrows-13.svg?stroke=#992f00'
 import logoImage from 'images/logo.svg'
 import screenshotImage from 'images/screenshot.svg'
 
+const isMobileVersion = window.innerWidth <= 800
 
 const sectionStyle: React.CSSProperties = {
   backgroundColor: colors.REDDISH_ORANGE,
   color: '#fff',
   fontFamily: 'ProximaSoft',
   overflow: 'hidden',
-  padding: '150px 20px 280px',
+  padding: isMobileVersion ? '200px 20px' : '150px 20px 280px',
   position: 'relative',
 }
 const contentStyle: React.CSSProperties = {
@@ -48,8 +49,8 @@ const orange6ArrowsStyle: React.CSSProperties = {
 }
 const orange13ArrowsStyle: React.CSSProperties = {
   position: 'absolute',
-  right: -80,
-  top: 60,
+  right: isMobileVersion ? 0 : -80,
+  top: isMobileVersion ? -160 : 60,
 }
 const screenshotStyle: React.CSSProperties = {
   borderRadius: 31,
@@ -58,6 +59,7 @@ const screenshotStyle: React.CSSProperties = {
   position: 'absolute',
   right: 15,
 }
+// TODO(cyrille): Handle pixel-high line between triangle and next section.
 const whiteTriangleStyle: React.CSSProperties = {
   borderBottom: 'solid #fff 10vw',
   borderLeft: 'solid transparent 100vw',
@@ -84,9 +86,9 @@ const HeaderSection = (): React.ReactElement => {
           $t(productName)
         </strong>
       </Trans>
-      <img src={orange6ArrowsImage} alt="" style={orange6ArrowsStyle} />
       <img src={orange13ArrowsImage} alt="" style={orange13ArrowsStyle} />
-      <img src={screenshotImage} style={screenshotStyle} alt="" />
+      {isMobileVersion ? null : <img src={orange6ArrowsImage} alt="" style={orange6ArrowsStyle} />}
+      {isMobileVersion ? null : <img src={screenshotImage} style={screenshotStyle} alt="" />}
     </div>
     <div style={whiteTriangleStyle} />
   </section>
