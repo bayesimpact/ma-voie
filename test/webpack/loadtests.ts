b8d4c586dcc3460b32c93308d74c2f7b43d7c017
Cyrille Corpet <cyrille@bayesimpact.org>
2020-07-21 11:50:52 +0200

    [Clean Code] Drop unused exports in i18n lib. (#57)

--- test/webpack/loadtests.ts
+++ test/webpack/loadtests.ts
@@ -2,7 +2,6 @@
 'use strict'
 require('@babel/polyfill')
 
-// TODO(cyrille): Add tests in the folder.
 
 // Add support for all files in the test directory.
 const testsContext = require.context('.', true, /_(test|helper)\.[jt]sx?$/)
