49dba6c865ff63eeecc7e2916c531f90396fa666
Émilie Guth <emilie@bayesimpact.org>
2020-07-23 15:53:38 +0200

    [Components] Header creation

---
+++ src/components/header.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {useTranslation} from 'react-i18next'
+
+import logoImage from 'images/logo-black.svg'
+
+
+const headerContainerStyle: React.CSSProperties = {
+  backgroundColor: '#fff',
+  position: 'fixed',
+  width: '100%',
+}
+const headerStyle: React.CSSProperties = {
+  boxShadow: '0 1px 0 0 rgb(229 230 230)',
+  display: 'flex',
+  padding: '15px 20px',
+}
+const logoStyle: React.CSSProperties = {
+  height: 30,
+  margin: 'auto',
+  width: 49,
+}
+
+const Header = (): React.ReactElement => {
+  const {t} = useTranslation()
+  return <div style={headerContainerStyle}>
+    <div style={headerStyle}>
+      <img src={logoImage} alt={t('productName')} style={logoStyle} />
+    </div>
+  </div>
+}
+
+
+export default React.memo(Header)
