804b0bd0c49672dd6ead5d47091341adef537986
Cyrille <cyrille@bayesimpact.org>
2020-07-23 15:33:46 +0200

    [Steps] Make a nice step UI.

--- src/components/pages/steps.tsx
+++ src/components/pages/steps.tsx
@@ -2,16 +2,23 @@ import React from 'react'
 import {useTranslation} from 'react-i18next'
 
 import Step from 'components/step'
+import competencesIcon from 'images/competences-ico.svg'
+import definitionIcon from 'images/definition-ico.svg'
+import formationIcon from 'images/formation-ico.svg'
+
 // This is a top level page and should never be nested in another one.
 // TOP LEVEL PAGE
 const StepsPage = (): React.ReactElement => {
   const {t} = useTranslation()
   // TODO(cyrille): Add header.
+  // TODO(cyrille): Add outer style to steps.
   // TODO(cyrille): Add step 4.
-  return <div>
-    <Step isOpen={true}>{t('Définition de votre projet')}</Step>
-    <Step>{t('Compétences')}</Step>
-    <Step>{t('Formations')}</Step>
+  return <div style={{fontFamily: 'ProximaSoft'}}>
+    <Step color={colors.LIGHT_TAN} icon={definitionIcon} isOpen={true} index={1}>
+      {t('Définition de votre projet')}
+    </Step>
+    <Step color={colors.SILVER} icon={competencesIcon} index={2}>{t('Compétences')}</Step>
+    <Step color={colors.LIGHT_SKY_BLUE} icon={formationIcon} index={3}>{t('Formations')}</Step>
   </div>
 }
 
--- src/components/step.tsx
+++ src/components/step.tsx
@@ -1,10 +1,85 @@
 import React from 'react'
+import {useTranslation} from 'react-i18next'
 
+import lockIcon from 'images/lock-ico.svg'
+
+const notAvailable = (): void => window.alert('Bientôt disponible...')
+
+const headerStyle: React.CSSProperties = {
+  alignItems: 'center',
+  backgroundColor: colors.TEAL_BLUE,
+  color: '#fff',
+  display: 'flex',
+  height: 40,
+  justifyContent: 'center',
+  textAlign: 'center',
+}
+const indexStyle: React.CSSProperties = {
+  color: colors.REDDISH_ORANGE,
+  margin: '0 auto',
+  textTransform: 'uppercase',
+}
+const titleStyle: React.CSSProperties = {
+  fontSize: 20,
+  fontWeight: 'bold',
+}
+const buttonStyle: React.CSSProperties = {
+  alignSelf: 'stretch',
+  backgroundColor: colors.REDDISH_ORANGE,
+  border: 0,
+  borderRadius: 25,
+  color: '#fff',
+  cursor: 'pointer',
+  fontSize: 18,
+  fontWeight: 'bold',
+  height: 50,
+}
+const forbiddenStyle: React.CSSProperties = {
+  fontSize: 15,
+  fontWeight: 'bold',
+}
 interface Props {
   children: React.ReactNode
+  color: string
+  icon: string
+  index: number
   isOpen?: boolean
+  style?: React.CSSProperties
 }
-const Step = ({children, isOpen}: Props): React.ReactElement =>
-  <div>{children}{isOpen ? ' Bientôt disponible...' : ''}</div>
-
+const Step = ({children, color, icon, index, isOpen, style}: Props): React.ReactElement => {
+  const {t} = useTranslation()
+  const containerStyle: React.CSSProperties = {
+    backgroundColor: '#fff',
+    border: `1px solid ${isOpen ? 'transparent' : colors.SILVER_TWO}`,
+    borderRadius: 20,
+    boxShadow: isOpen ? '0 4px 24px 0 rgba(0,0,0,.2)' : 'initial',
+    fontSize: 13,
+    overflow: 'hidden',
+    ...style,
+  }
+  const contentStyle: React.CSSProperties = {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    padding: isOpen ? '25px 30px 30px' : 35,
+  }
+  const iconStyle: React.CSSProperties = {
+    backgroundColor: color,
+    borderRadius: 35,
+    height: 70,
+    margin: 20,
+    width: 70,
+  }
+  return <div style={containerStyle}>
+    {isOpen ? <div style={headerStyle}>{t('En cours')}</div> : null}
+    <div style={contentStyle}>
+      <div style={indexStyle}>{t('étape {{index}}', {index})}</div>
+      <div style={titleStyle}>{children}</div>
+      <img style={iconStyle} src={isOpen ? icon : lockIcon} alt="" />
+      {isOpen ? <button onClick={notAvailable} style={buttonStyle}>{t('Commencez')}</button> :
+        // TODO(cyrille): Replace with the list of steps to complete beforehand.
+        <span style={forbiddenStyle}>{t("Terminez l'étape précédente", {count: index - 1})}</span>}
+    </div>
+  </div>
+}
 export default React.memo(Step)
