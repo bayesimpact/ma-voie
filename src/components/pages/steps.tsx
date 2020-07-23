7db25d7e6fa291d2595890d8cdb2e53d1af8fb17
Cyrille Corpet <cyrille@bayesimpact.org>
2020-07-23 12:05:39 +0200

    [Workflow] Add a skeleton step component (#84)

--- src/components/pages/steps.tsx
+++ src/components/pages/steps.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import {useTranslation} from 'react-i18next'
 
+import Step from 'components/step'
 // This is a top level page and should never be nested in another one.
 // TOP LEVEL PAGE
 const StepsPage = (): React.ReactElement => {
-  return <div>Bientôt disponible&hellip;</div>
+  const {t} = useTranslation()
+  // TODO(cyrille): Add header.
+  // TODO(cyrille): Add step 4.
+  return <div>
+    <Step isOpen={true}>{t('Définition de votre projet')}</Step>
+    <Step>{t('Compétences')}</Step>
+    <Step>{t('Formations')}</Step>
+  </div>
 }
 
 export default React.memo(StepsPage)
