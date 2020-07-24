9de3091b6ed4b0d90c74db3e2c4f0d31c798254a
Cyrille Corpet <cyrille@bayesimpact.org>
2020-07-24 10:59:57 +0200

    [Terms] Add real links in terms. PATH=/cgu (#95)
    * [Terms] Add real links in terms.
    PATH=/cgu

--- src/components/pages/terms.tsx
+++ src/components/pages/terms.tsx
@@ -50,7 +50,7 @@ const linkStyle: React.CSSProperties = {
 
 
 const CustomLink = <T extends Record<string, unknown>>(props: T): React.ReactElement => {
-  return <a {...props} rel="noopener noreferer" blank="_target" style={linkStyle} />
+  return <a {...props} rel="noopener noreferer" target="_blank" style={linkStyle} />
 }
 
 
@@ -123,9 +123,9 @@ const TermsPage = (): React.ReactElement => {
             '- 01 46 17 10 00',
           owner: 'Chance / MaVoie.org',
           productName: t('productName').toLocaleUpperCase(),
-          publisher: `MaVoie.org – ${email} Le responsable publication ` +
+          publisher: `MaVoie.org – [${email}](mailto:${email}) Le responsable publication ` +
           'est une personne physique ou une personne morale.',
-          webmaster: `John Métois – ${email}`,
+          webmaster: `John Métois – [${email}](mailto:${email})`,
         })} renderers={renderers} />
       </div>
     </div>
