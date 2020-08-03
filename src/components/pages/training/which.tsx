724b791087bec3f149c0802d477f13d06d33fdc2
Émilie Guth <emilie@bayesimpact.org>
2020-08-03 16:42:56 +0200

    [Section training] Screens and partners (looks like the one in definition but the content should be different) PATH=/formation/quelle

---
+++ src/components/pages/training/which.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import {Trans, useTranslation} from 'react-i18next'
+import {Link} from 'react-router-dom'
+
+import {getPath} from 'store/url'
+
+import Button from 'components/button'
+import Layout from 'components/layout'
+import Tip from 'components/tip'
+
+const containerStyle: React.CSSProperties = {
+  color: colors.DARK_FOREST_GREEN,
+  marginTop: 40,
+}
+const titleStyle: React.CSSProperties = {
+  fontSize: 22,
+}
+const linkStyle: React.CSSProperties = {
+  textDecoration: 'none',
+}
+
+// This is a top level page and should never be nested in another one.
+// TOP LEVEL PAGE
+const TrainingWhichPage = (): React.ReactElement => {
+  const {t} = useTranslation()
+  return <Layout header={t('Formation')}>
+    <Trans style={containerStyle}>
+      <h1 style={titleStyle}>Quelle formation&nbsp;?</h1>
+      <p>
+        Choisissez une formation en fonction de ce qui vous plaît,
+        vos capacités financières, et des débouchés.
+      </p>
+    </Trans>
+    <Tip title={t('Astuce')}>
+      <Trans>Vous pouvez utiliser votre crédit CPF pour financer votre formation.</Trans>
+    </Tip>
+    <Link to={getPath('TRAINING_PARTNERS_INTERNAL', t)} style={linkStyle} >
+      <Button bgColor={colors.REDDISH_ORANGE} >{t('Commencer')}</Button>
+    </Link>
+  </Layout>
+}
+
+export default React.memo(TrainingWhichPage)
