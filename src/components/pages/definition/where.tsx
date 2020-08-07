2b9b0f75b9abd93d55863d5d0b7e8c58efbfb80d
Émilie Guth <emilie@bayesimpact.org>
2020-08-07 17:25:52 +0200

    Revert "[Form buttons] Factorize button bases into button.tsx and add a link in definition/quoi"
    This reverts commit 48da19a109803aeac00ec53789d86b7568d3454c.

--- src/components/pages/definition/where.tsx
+++ src/components/pages/definition/where.tsx
@@ -1,32 +1,58 @@
 import React, {useCallback} from 'react'
 import {useTranslation} from 'react-i18next'
+import {Link} from 'react-router-dom'
 
 import {updateProject, useDispatch} from 'store/actions'
 import {prepareT} from 'store/i18n'
 import {useProjectId} from 'store/selections'
-import {Page} from 'store/url'
+import {Page, getPath} from 'store/url'
 
-import SelectButton from 'components/select_button'
+import Button from 'components/button'
 import Layout from 'components/layout'
+
+const linkStyle: React.CSSProperties = {
+  textDecoration: 'none',
+}
+const buttonContainerStyle: React.CSSProperties = {
+  fontSize: 16,
+  paddingBottom: 20,
+}
 
 interface ButtonProps {
   hasDefinedProject: boolean
   name: string
-  page: Page
+  redirect: Page
 }
 // TODO(cyrille): Make a <select> component with button options.
 const BUTTONS: readonly ButtonProps[] = [
   {
     hasDefinedProject: true,
     name: prepareT('Je sais ce que je veux faire'),
-    page: 'DEFINITION_WHAT',
+    redirect: 'DEFINITION_WHAT',
   },
   {
     hasDefinedProject: false,
     name: prepareT('Je ne sais pas / Je suis perdu·e'),
-    page: 'DEFINITION_LOST',
+    redirect: 'DEFINITION_LOST',
   },
 ]
+
+const SelectButtonBase = ({name, redirect, hasDefinedProject}: ButtonProps): React.ReactElement => {
+  const dispatch = useDispatch()
+  const projectId = useProjectId()
+  const [translate] = useTranslation()
+  const onClick = useCallback(() => {
+    dispatch(updateProject({hasDefinedProject, projectId}))
+  }, [dispatch, hasDefinedProject, projectId])
+  return <div style={buttonContainerStyle}>
+    <Link to={getPath(redirect, translate)} style={linkStyle}>
+      <Button type="variable" onClick={onClick}>
+        {translate(name)}
+      </Button>
+    </Link>
+  </div>
+}
+const SelectButton = React.memo(SelectButtonBase)
 
 // This is a top level page and should never be nested in another one.
 // TOP LEVEL PAGE
@@ -34,19 +60,10 @@ const WherePage = (): React.ReactElement => {
   const {t} = useTranslation()
   const title = t('Où en êtes-vous de votre projet professionnel\u00A0?')
 
-  const dispatch = useDispatch()
-  const projectId = useProjectId()
-
-  const onClick = useCallback((hasDefinedProject: boolean): void => {
-    dispatch(updateProject({hasDefinedProject, projectId}))
-  }, [dispatch, projectId])
-
   // FIXME(émilie): Delete links and change them by the good handler
   return <Layout header={t('Définition')} title={title}>
-    {BUTTONS.map((props: ButtonProps) => <SelectButton<boolean>
-      onClick={onClick} key={props.page}
-      name={props.name} page={props.page} value={props.hasDefinedProject} />,
-    )}
+    {BUTTONS.map((props: ButtonProps) =>
+      <SelectButton {...props} key={props.redirect} />)}
   </Layout>
 }
 
