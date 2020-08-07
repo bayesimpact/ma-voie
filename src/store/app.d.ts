d6c6e34f8e16a17d009f683a0547a72081f8d64d
Émilie Guth <emilie@bayesimpact.org>
2020-08-07 17:29:07 +0200

    Revert "Revert "[Form buttons] Factorize button bases into button.tsx and add a link in definition/quoi""
    This reverts commit 2b9b0f75b9abd93d55863d5d0b7e8c58efbfb80d.

--- src/store/app.d.ts
+++ src/store/app.d.ts
@@ -2,15 +2,26 @@
 // TODO(cyrille): Use protobuffers.
 declare namespace bayes {
   namespace maVoie {
+
     interface User {
       lastName?: string
       name?: string
       projects?: readonly Project[]
     }
 
+    type ProjectExperience =
+      | 'new'
+      | '1-3'
+      | '3-5'
+      | '5'
+    type ProjectObjective =
+      | 'job'
+      | 'training'
+
     interface Project {
-      experience?: 'new' | '1-3' | '3-5' | '5'
+      experience?: ProjectExperience
       hasDefinedProject?: boolean
+      objective?: ProjectObjective
       projectId: string
     }
 
