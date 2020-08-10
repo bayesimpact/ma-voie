e57cfa7871165674e59d55938066e1a63d9f401d
Cyrille <cyrille@bayesimpact.org>
2020-08-10 15:06:21 +0200

    [Partners] Make the partners internal page step-agnostic.

--- src/components/pages/steps.tsx
+++ src/components/pages/steps.tsx
@@ -1,3 +1,4 @@
+import _keyBy from 'lodash/keyBy'
 import ArrowDownIcon from 'mdi-react/ArrowDownIcon'
 import React, {useCallback, useEffect, useState} from 'react'
 import {useTranslation} from 'react-i18next'
@@ -15,18 +16,32 @@ import competencesIcon from 'images/competences-ico.svg'
 import definitionIcon from 'images/definition-ico.svg'
 import formationIcon from 'images/formation-ico.svg'
 
-const STEPS = [
+interface StepInfo {
+  color: string
+  icon: string
+  isOpen?: boolean
+  page: Page
+  shortTitle?: string
+  stepId: 'definition' | 'skills' | 'training'
+  title: string
+}
+
+// TODO(cyrille): Move to store.
+const STEPS: readonly StepInfo[] = [
   {
     color: colors.LIGHT_TAN,
     icon: definitionIcon,
     isOpen: true,
     page: ['DEFINITION'],
+    shortTitle: prepareT('Définition'),
+    stepId: 'definition',
     title: prepareT('Définition de votre projet'),
   },
   {
     color: colors.SILVER,
     icon: competencesIcon,
     page: ['SKILLS_LIST'],
+    stepId: 'skills',
     title: prepareT('Compétences'),
   },
   {
@@ -34,9 +49,12 @@ const STEPS = [
     icon: formationIcon,
     // TODO(émilie): put the good path when reviewed
     page: ['SKILLS_LIST'],
+    stepId: 'training',
     title: prepareT('Formations'),
   },
-] as const
+]
+export const STEPS_BY_ID = _keyBy(STEPS, 'stepId')
+
 const stepsStyle: React.CSSProperties = {
   padding: '30px 0',
 }
