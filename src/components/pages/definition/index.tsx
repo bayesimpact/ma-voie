e776cdaabe5db622e4437faca0dc2be3784a7463
Cyrille <cyrille@bayesimpact.org>
2020-08-11 12:36:30 +0200

    wip

--- src/components/pages/definition/index.tsx
+++ src/components/pages/definition/index.tsx
@@ -8,9 +8,8 @@ import GoPage from './go'
 import InterestPage from './interest'
 import JobPage from './job'
 import LostPage from './lost'
-import DefinitionPartnersExternalPage from './partners_external'
 // TODO(cyrille): Move to pages.
-import PartnersInternalPage from '../training/partners_internal'
+import PartnersPage from '../training/partners_internal'
 import RedefinePage from './redefine'
 import WhatPage from './what'
 import WherePage from './where'
@@ -26,10 +25,10 @@ const DefinitionPage = (): React.ReactElement => {
     <Route path={defineAndGetPath('LOST')} component={LostPage} />
     <Route
       path={defineAndGetPath('PARTNERS_EXTERNAL')}
-      component={DefinitionPartnersExternalPage} />
+      component={PartnersPage} />
     <Route
       path={defineAndGetPath('PARTNERS_INTERNAL')}
-      component={PartnersInternalPage} />
+      component={PartnersPage} />
     <Route path={defineAndGetPath('REDEFINE')} component={RedefinePage} />
     <Route path={defineAndGetPath('WHAT')} component={WhatPage} />
     <Route path={defineAndGetPath('WHERE')} component={WherePage} />
--- src/components/pages/definition/partners_external.tsx
+++ src/components/pages/definition/partners_external.tsx
@@ -1,40 +1,18 @@
-import React, {useCallback, useState} from 'react'
-import {useTranslation, Trans} from 'react-i18next'
+// TODO(cyrille): Move to components.
+import React, {useCallback} from 'react'
+import {useTranslation} from 'react-i18next'
 import ReactMarkdown from 'react-markdown'
 import {Link} from 'react-router-dom'
 
 import {getPath} from 'store/url'
 
 import Button from 'components/button'
-import Layout from 'components/layout'
 import {PartnerProps} from 'components/partner_card'
-import TabsNav, {TabProps} from 'components/tabs_nav'
 
-import logoChance from 'images/logo-chance.svg'
 
-const description = `Ce cours en ligne* "Focus compétences" est composé de 4 séquences :
-
-1. Définir la notion de compétence
-
-2. Acquérir une méthodologie pour identifier ses compétences
-
-3. Valoriser ses compétences auprès d'un recruteur
-
-4. Augmenter sa visibilité et sa crédibilité sur les réseaux sociaux
-
-Chaque séquence est composée de vidéos, quiz, et de ressources complémentaires**.
-Vous pouvez suivre chaque séquence à votre rythme et selon vos besoins.`
 
 const linkStyle: React.CSSProperties = {
   textDecoration: 'none',
-}
-const buttonWrapperStyle: React.CSSProperties = {
-  marginBottom: 55,
-  padding: '10px 20px 0',
-}
-const paragrapheStyle: React.CSSProperties = {
-  fontSize: 14,
-  padding: '0 20px',
 }
 const partnerContainerStyle: React.CSSProperties = {
   borderBottom: `2px solid ${colors.SILVER_TWO}`,
@@ -97,38 +75,7 @@ const buttonsContainerStyle: React.CSSProperties = {
 const partnerButtonStyle: React.CSSProperties = {
   width: 145,
 }
-// TODO(émilie): set the partners
-const partners: readonly PartnerProps[] = [
-  {
-    description,
-    details: 'MOOC',
-    logo: logoChance,
-    name: 'Externe 1',
-    partnerId: 'chance-1',
-    url: '',
-  },
-  {
-    description,
-    details: 'MOOC',
-    logo: logoChance,
-    name: 'Externe 2',
-    partnerId: 'chance-2',
-    url: '',
-  },
-]
 
-const tabs: readonly TabProps[] = [
-  {
-    redirect: ['DEFINITION', 'PARTNERS_INTERNAL'],
-    title: <Trans>Certifiés par Ma Voie</Trans>,
-  },
-  {
-    redirect: ['DEFINITION', 'PARTNERS_EXTERNAL'],
-    title: <Trans>Autres partenaires</Trans>,
-  },
-]
-
-// TODO(cyrille): Move to its own module.
 interface ExternalPartnerProps extends PartnerProps {
   isOpen: boolean
   onSelect: (id: string) => void
@@ -190,36 +137,4 @@ const ExternalPartner = (props: ExternalPartnerProps): React.ReactElement => {
   </div>
 }
 
-// This is a top level page and should never be nested in another one.
-// TOP LEVEL PAGE
-const DefinitionPartnersExternalPage = (): React.ReactElement => {
-  const {t} = useTranslation()
-  const bigTitle = t('Voici les partenaires idéaux pour vous aider')
-  const [openPartnerId, setOpenPartnerId] = useState<null|string>(null)
-
-  const onSelect = useCallback((partnerId: string): void =>
-    setOpenPartnerId(previousPartnerId => previousPartnerId === partnerId ? null : partnerId), [])
-
-  // FIXME(émilie): Change links to redirect where it is needed
-  return <Layout header={t('Définition')} bigTitle={bigTitle}>
-    <TabsNav tabs={tabs} />
-    {partners.map((partner) => {
-      return <ExternalPartner
-        key={partner.partnerId} {...partner}
-        isOpen={openPartnerId === partner.partnerId} onSelect={onSelect} />
-    })}
-    <Trans parent="p" style={paragrapheStyle}>
-      Si vous pensez avoir déjà réussi cette étape, cliquez sur
-      "Je l'ai fait moi-même" pour passer à l'étape suivante.
-    </Trans>
-    <div style={buttonWrapperStyle}>
-      <Link to={getPath(['DEFINITION', 'WHAT'], t)} style={linkStyle}>
-        <Button type="specific">
-          {t('Je l\'ai fait moi-même')}
-        </Button>
-      </Link>
-    </div>
-  </Layout>
-}
-
-export default React.memo(DefinitionPartnersExternalPage)
+export default React.memo(ExternalPartner)
