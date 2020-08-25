10e371e89b7845d81e96b66050dcfd0819d4046c
Émilie Guth <emilie@bayesimpact.org>
2020-08-25 14:56:28 +0200

    [Login] Create email / facebook / google buttons and signup page PATH=/inscription Buttons can also be seen in /components page.

---
+++ src/components/button_icon.tsx
@@ -0,0 +1,90 @@
+import React from 'react'
+
+import iconEmail from 'images/mail-ico.png'
+import iconFacebook from 'images/facebook-ico.png'
+import iconGoogle from 'images/google-ico.png'
+
+const buttonStyle: React.CSSProperties = {
+  alignItems: 'center',
+  alignSelf: 'stretch',
+  border: 0,
+  borderRadius: 25,
+  display: 'flex',
+  fontFamily: 'ProximaSoft',
+  fontSize: 15,
+  fontWeight: 'normal',
+  height: 50,
+  justifyContent: 'center',
+  paddingLeft: 0,
+  position: 'relative',
+  textAlign: 'center',
+}
+
+const buttonsStyle = {
+  email: {
+    ...buttonStyle,
+    backgroundColor: colors.TEAL_BLUE,
+    color: '#fff',
+  },
+  facebook: {
+    ...buttonStyle,
+    backgroundColor: colors.FACEBOOK,
+    color: '#fff',
+  },
+  google: {
+    ...buttonStyle,
+    backgroundColor: '#fff',
+    borderColor: colors.DARK_FOREST_GREEN,
+    borderStyle: 'solid',
+    borderWidth: 0.5,
+    color: colors.DARK_FOREST_GREEN,
+  },
+} as const
+
+export type ButtonIconType = keyof typeof buttonsStyle
+
+const iconBaseStyle: React.CSSProperties = {
+  left: 20,
+  position: 'absolute',
+  top: 16,
+}
+
+const iconStyle = {
+  email: iconBaseStyle,
+  facebook: {
+    ...iconBaseStyle,
+    left: 24,
+  },
+  google: iconBaseStyle,
+} as const
+
+const iconImage = {
+  email: iconEmail,
+  facebook: iconFacebook,
+  google: iconGoogle,
+} as const
+
+const spanStyle: React.CSSProperties = {
+  margin: 'auto',
+}
+
+interface ButtonProps {
+  children: React.ReactNode
+  onClick?: (event: React.MouseEvent<HTMLDivElement>) => void
+  style?: React.CSSProperties
+  type: ButtonIconType
+}
+
+const ButtonIcon = ({children, onClick, style, type}: ButtonProps): React.ReactElement => {
+  const buttonFinalStyle: React.CSSProperties = {
+    ...onClick ? {cursor: 'pointer'} : {},
+    ...buttonsStyle[type],
+    ...style,
+  }
+  return <div onClick={onClick} style={buttonFinalStyle}>
+    <img src={iconImage[type]} alt="" style={iconStyle[type]} />
+    <span style={spanStyle}>{children}</span>
+  </div>
+}
+
+export default React.memo(ButtonIcon)
--- src/components/pages/components.tsx
+++ src/components/pages/components.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Button from 'components/button'
+import ButtonIcon from 'components/button_icon'
 import Layout from 'components/layout'
 import Tip from 'components/tip'
 
@@ -11,17 +12,23 @@ const onClick = (): void => window.alert('Bientôt disponible...')
 const ComponentsPage = (): React.ReactElement => {
   return <Layout header="Header title" title="Layout title" bigTitle="Layout big title">
     <br />
-    <Button type="firstLevel" onClick={onClick} >Button firstLevel</Button>
+    <Button type="firstLevel" onClick={onClick}>Button firstLevel</Button>
     <br />
-    <Button type="secondLevel" onClick={onClick} >Button secondLevel</Button>
+    <Button type="secondLevel" onClick={onClick}>Button secondLevel</Button>
     <br />
-    <Button type="variable" onClick={onClick} >Button variable</Button>
+    <Button type="variable" onClick={onClick}>Button variable</Button>
     <br />
-    <Button type="discret" onClick={onClick} >Button discret</Button>
+    <Button type="discret" onClick={onClick}>Button discret</Button>
     <br />
-    <Button type="specific" onClick={onClick} >Button specific</Button>
+    <Button type="specific" onClick={onClick}>Button specific</Button>
     <br />
-    <Button type="menu" onClick={onClick} >Button menu</Button>
+    <Button type="menu" onClick={onClick}>Button menu</Button>
+    <br />
+    <ButtonIcon type="email" onClick={onClick}>Button Email</ButtonIcon>
+    <br />
+    <ButtonIcon type="facebook" onClick={onClick}>Button Facebook</ButtonIcon>
+    <br />
+    <ButtonIcon type="google" onClick={onClick}>Button Google</ButtonIcon>
     <br />
     <Tip title="Astuce">
       Vous pouvez utiliser votre crédit CPF pour financer votre formation.
