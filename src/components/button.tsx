0d9bbc3437473885747baf0d3117a6cec2129893
Émilie Guth <emilie@bayesimpact.org>
2020-08-25 14:09:47 +0200

    [Login] Create email / facebook / google buttons and signup page PATH=/inscription Buttons can also be seen in /components page.

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 import {colorToAlpha} from 'components/colors'
 
+import iconEmail from 'images/mail-ico.png'
+import iconFacebook from 'images/facebook-ico.png'
+import iconGoogle from 'images/google-ico.png'
+
 const buttonStyle: React.CSSProperties = {
   alignItems: 'center',
   alignSelf: 'stretch',
@@ -22,10 +26,40 @@ const buttonsStyle = {
     backgroundColor: '#fff',
     color: colors.DARK_FOREST_GREEN,
   },
+  email: {
+    ...buttonStyle,
+    backgroundColor: colors.TEAL_BLUE,
+    color: '#fff',
+    fontSize: 15,
+    fontWeight: 'normal',
+    paddingLeft: 0,
+    position: 'relative',
+  },
+  facebook: {
+    ...buttonStyle,
+    backgroundColor: colors.FACEBOOK,
+    color: '#fff',
+    fontSize: 15,
+    fontWeight: 'normal',
+    paddingLeft: 0,
+    position: 'relative',
+  },
   firstLevel: {
     ...buttonStyle,
     backgroundColor: colors.REDDISH_ORANGE,
     color: '#fff',
+  },
+  google: {
+    ...buttonStyle,
+    backgroundColor: '#fff',
+    borderColor: colors.DARK_FOREST_GREEN,
+    borderStyle: 'solid',
+    borderWidth: 0.5,
+    color: colors.DARK_FOREST_GREEN,
+    fontSize: 15,
+    fontWeight: 'normal',
+    paddingLeft: 0,
+    position: 'relative',
   },
   menu: {
     ...buttonStyle,
@@ -70,6 +104,22 @@ const buttonsStyle = {
 
 export type ButtonType = keyof typeof buttonsStyle
 
+const iconStyle: React.CSSProperties = {
+  left: 20,
+  position: 'absolute',
+  top: 16,
+}
+
+const facebookIconStyle: React.CSSProperties = {
+  left: 24,
+  position: 'absolute',
+  top: 16,
+}
+
+const spanStyle: React.CSSProperties = {
+  margin: 'auto',
+}
+
 interface ButtonProps {
   children: React.ReactNode
   onClick?: (event: React.MouseEvent<HTMLDivElement>) => void
@@ -77,13 +127,33 @@ interface ButtonProps {
   type: ButtonType
 }
 
+const renderChildren = (type: ButtonType, children: React.ReactNode): React.ReactNode => {
+  switch (type) {
+    case 'email':
+      return <React.Fragment>
+        <img src={iconEmail} alt="Google" style={iconStyle} />
+        <span style={spanStyle}>{children}</span>
+      </React.Fragment>
+    case 'facebook':
+      return <React.Fragment>
+        <img src={iconFacebook} alt="Google" style={facebookIconStyle} />
+        <span style={spanStyle}>{children}</span>
+      </React.Fragment>
+    case 'google':
+      return <React.Fragment>
+        <img src={iconGoogle} alt="Google" style={iconStyle} />
+        <span style={spanStyle}>{children}</span>
+      </React.Fragment>
+  }
+  return children
+}
 const Button = ({children, onClick, style, type}: ButtonProps): React.ReactElement => {
   const buttonFinalStyle: React.CSSProperties = {
     ...onClick ? {cursor: 'pointer'} : {},
     ...buttonsStyle[type],
     ...style,
   }
-  return <div onClick={onClick} style={buttonFinalStyle}>{children}</div>
+  return <div onClick={onClick} style={buttonFinalStyle}>{renderChildren(type, children)}</div>
 }
 
 export default React.memo(Button)
