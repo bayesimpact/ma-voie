e45a4e5f7bc6e9f5d4368c3c03b9920deb4d019f
Émilie Guth <emilie@bayesimpact.org>
2020-08-26 17:43:25 +0200

    [Password] Forgot password, step 1 : form to send an email. Step 2 (to be done) : create the page linked in the email. PATH=/connexion

---
+++ src/components/pages/forgot_password.tsx
@@ -0,0 +1,64 @@
+import React, {useCallback, useState} from 'react'
+import {useTranslation} from 'react-i18next'
+
+import {FirebaseAuth} from 'database/firebase'
+
+import Button from 'components/button'
+import Input from 'components/input'
+import Layout from 'components/layout'
+
+const inputStyle: React.CSSProperties = {
+  border: `1px solid ${colors.SILVER_THREE}`,
+  borderRadius: 15,
+  fontFamily: 'ProximaSoft',
+  fontSize: 16,
+  height: 24,
+  lineHeight: 24,
+  marginTop: 20,
+  padding: '18px 25px',
+  width: 'calc(100% - 52px)',
+}
+
+const buttonStyle: React.CSSProperties = {
+  marginTop: 20,
+}
+
+const errorStyle: React.CSSProperties = {
+  color: colors.RED_ERROR,
+  fontSize: 13,
+  marginLeft: 25,
+}
+
+
+// This is a top level page and should never be nested in another one.
+// TOP LEVEL PAGE
+const ForgotPasswordPage = (): React.ReactElement => {
+  const {t} = useTranslation()
+  const [email, setEmail] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [sent, setSent] = useState(false)
+
+  const onSubmit = useCallback(() => {
+    FirebaseAuth.sendPasswordResetEmail(email).
+      then(() => {
+        setSent(true)
+      }).catch((error) => {
+        setErrorMessage(error.message)
+      })
+  }, [email, setErrorMessage])
+
+  return <Layout bigTitle={t('Mot de passe oublié')} menu="site">
+    <form onSubmit={onSubmit}>
+      <Input
+        placeholder={t('Email')} style={inputStyle}
+        autoComplete="email" onChange={setEmail} />
+      <Button type="secondLevel" onClick={onSubmit} style={buttonStyle} >
+        {t('Valider')}
+      </Button>
+      {sent ? t('Un email pour réinitialiser votre mot de passe vous a été envoyé.') : null}
+      {errorMessage ? <div style={errorStyle}>{errorMessage}</div> : null}
+    </form>
+  </Layout>
+}
+
+export default React.memo(ForgotPasswordPage)
