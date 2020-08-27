aceec172894410de6c0fc04b777cae0c83eab35f
Cyrille Corpet <cyrille@bayesimpact.org>
2020-08-27 17:52:32 +0200

    [Skills] Fetch skills from prod assets. (#244)

--- src/components/pages/skills/list.tsx
+++ src/components/pages/skills/list.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react'
+import React, {useCallback, useEffect, useMemo, useState} from 'react'
 import {useTranslation} from 'react-i18next'
 import {useHistory} from 'react-router'
 
@@ -25,82 +25,6 @@ interface SkillType {
   name: string
 }
 
-// TODO(émilie): Move into another file
-// FIXME(émilie): Select by rome ID first
-const skillsByOgr: readonly SkillType[] = [
-  {
-    codeOgr: '101343',
-    isPriority: true,
-    name: 'Principes de la relation client',
-  },
-  {
-    codeOgr: '123058',
-    isPriority: true,
-    name: 'Techniques de séchage',
-  },
-  {
-    codeOgr: '101908',
-    isPriority: true,
-    name: 'Manipulation d\'outil tranchant (ciseaux ...)',
-  },
-  {
-    codeOgr: '101909',
-    isPriority: true,
-    name: 'Diagnostic capillaire',
-  },
-  {
-    codeOgr: '101910',
-    isPriority: true,
-    name: 'Techniques de coupes de cheveux',
-  },
-  {
-    codeOgr: '101912',
-    isPriority: true,
-    name: 'Coiffure enfant',
-  },
-  {
-    codeOgr: '101913',
-    isPriority: true,
-    name: 'Coiffure femme',
-  },
-  {
-    codeOgr: '101914',
-    isPriority: true,
-    name: 'Coiffure homme',
-  },
-  {
-    codeOgr: '101915',
-    isPriority: true,
-    name: 'Techniques de décoloration, de coloration de cheveux...',
-  },
-  {
-    codeOgr: '101923',
-    isPriority: true,
-    name: 'Techniques de pose de perruques',
-  },
-  {
-    codeOgr: '119893',
-    isPriority: true,
-    name: 'Techniques de raccord, de rajouts, d\'extensions',
-  },
-  {
-    codeOgr: '118975',
-    name: 'Outils bureautiques',
-  },
-  {
-    codeOgr: '120889',
-    name: 'Gestion administrative',
-  },
-  {
-    codeOgr: '120888',
-    name: 'Gestion comptable',
-  },
-  {
-    codeOgr: '100090',
-    name: 'Techniques pédagogiques',
-  },
-]
-
 // This is a top level page and should never be nested in another one.
 // TOP LEVEL PAGE
 const SkillsListPage = (): React.ReactElement => {
@@ -111,28 +35,37 @@ const SkillsListPage = (): React.ReactElement => {
   const projectId = useProjectId()
   const project = useProject()
   const history = useHistory()
+  const {job: {jobGroup: {romeId = ''} = {}} = {}} = project
+  const [skillsList, setSkillsList] = useState<readonly SkillType[]>([])
+  useEffect((): void => {
+    if (!romeId) {
+      return
+    }
+    // TODO(cyrille): Rather fetch from static values in production assets.
+    import(`skills/skills_${romeId}.json`).then(({default: list}) => setSkillsList(list))
+  }, [romeId])
 
   const [valuesSelected, setValuesSelected] = useState<readonly string[]>(project.skills || [])
 
   const handleClick = useCallback((): void => {
     dispatch(updateProject({projectId, skills: valuesSelected}))
     // TODO(émilie): validate when all the priority items are checked
-    if (valuesSelected.length === skillsByOgr.length) {
+    if (valuesSelected.length === skillsList.length) {
       history.push(getPath(['SKILLS', 'GO'], t))
     } else {
       history.push(getPath(['SKILLS', 'TRAINING'], t))
     }
-  }, [dispatch, history, projectId, t, valuesSelected])
+  }, [dispatch, history, projectId, skillsList.length, t, valuesSelected])
 
-  const checkboxListData = skillsByOgr.map(({codeOgr, isPriority = false, name}) =>
+  const checkboxListData = useMemo(() => skillsList.map(({codeOgr, isPriority, name}) =>
     isPriority
       ? {label: <span style={priorityStyle}>{name}</span>, value: codeOgr}
       : {label: name, value: codeOgr},
-  )
+  ), [skillsList])
 
   return <Layout header={t('Compétences')} title={title}>
-    <CheckboxList list={checkboxListData}
-      valuesSelected={valuesSelected} onChange={setValuesSelected} />
+    <CheckboxList
+      list={checkboxListData} valuesSelected={valuesSelected} onChange={setValuesSelected} />
     <div style={buttonContainerStyle}>
       <Button type="secondLevel" onClick={handleClick}>
         {t('Valider')}
