595512e42a55d563707edd1d54e97ab02baa8276
Cyrille Corpet <cyrille@bayesimpact.org>
2020-08-31 11:11:20 +0200

    [Firebase] Move config to environment file. (#266)

--- release/entrypoint.sh
+++ release/entrypoint.sh
@@ -14,6 +14,13 @@ replace_string 'environment:"production"' 'environment:"demo"' "$JS_APP_FILES"
 
 # TODO(pascal): Read from const.json5 and const_dist.json5
 replace_string '6d5d1f5cb15e3cc946cbe3783e5f9e7f' '174aeb9469460975d8d6a14b31b6f916' "$JS_APP_FILES"
+replace_string 'AIzaSyAGH7JHYblSrKJsBSdr5UG0ZEBNdj8QRBM' 'AIzaSyCPdmI7WHWy6McHgu53OdW_QToZCAxMuT8' "$JS_APP_FILES"
+replace_string 'ma-voie.firebaseapp.com' 'ma-voie-demo.firebaseapp.com' "$JS_APP_FILES"
+replace_string 'https://ma-voie.firebaseio.com' 'https://ma-voie-demo.firebaseio.com' "$JS_APP_FILES"
+replace_string 'ma-voie.appspot.com' 'ma-voie-demo.appspot.com' "$JS_APP_FILES"
+replace_string '"projectId":"ma-voie"' '"projectId":"ma-voie-demo"' "$JS_APP_FILES"
+replace_string '1:740445000971:web:f73319eae6caf130c5be0e' '1:1010721781393:web:a125d44474595b3ad67130' "$JS_APP_FILES"
+replace_string '740445000971' '1010721781393' "$JS_APP_FILES"
 
 # TODO(pascal): Try to find a way to run the command if any.
 nginx -g 'daemon off;'
