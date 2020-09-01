9a6e19422901c8fe609ef0d25495f0a1f274a61c
Cyrille Corpet <cyrille@bayesimpact.org>
2020-09-01 09:26:32 +0200

    [Server] Add a basic authentication. (#277)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,19 @@
 import * as functions from 'firebase-functions'
-import {Express} from 'express'
+import {Express, Response} from 'express'
+import * as BasicAuth from 'express-basic-auth'
 
 const express = require('express')
 
 const app: Express = express()
 
-app.get('/', (request, response) => {
+app.use(BasicAuth({
+  challenge: true,
+  realm: 'MaVoie',
+  users: functions.config().basicauth,
+}))
+
+app.get('/', (request: BasicAuth.IBasicAuthedRequest, response: Response) => {
   functions.logger.info('Hello logs!', {structuredData: true})
-  response.send('Hello from Firebase!')
+  response.send(`Hello ${request.auth.user} from Firebase!`)
 })
 export const user = functions.https.onRequest(app)
