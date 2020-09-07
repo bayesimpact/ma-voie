33b68eeaa73a9c7cfbfa33b482cc2e09c7b8cf57
Émilie Guth <emilie.guth@gmail.com>
2020-09-07 16:28:45 +0200

    [Firestore] Store projects under user document (#315)
    Fixes #312

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -4,47 +4,12 @@ import {firebaseReducer} from 'react-redux-firebase'
 import {actionTypes, firestoreReducer} from 'redux-firestore'
 
 export type AllActions =
-  | CreateProject
   | Logout
-  | UpdateProject
-  | UpdateUser
-  | UpdateStep
   | ClearData
 
 // Type of the main dispatch function.
 export type DispatchAllActions = Dispatch<AllActions>
 export const useDispatch = (): DispatchAllActions => genericUseDispatch<DispatchAllActions>()
-
-interface UpdateUser extends Readonly<Action<'UPDATE_USER'>> {
-  user: Partial<bayes.maVoie.User>
-}
-
-// TODO(émilie): Delete as it is no more used.
-function updateUser(user: Partial<bayes.maVoie.User>): UpdateUser {
-  return {type: 'UPDATE_USER', user}
-}
-
-type CreateProject = Readonly<Action<'CREATE_PROJECT'>>
-const createProjectAction: CreateProject = {type: 'CREATE_PROJECT'}
-
-interface UpdateProject extends Readonly<Action<'UPDATE_PROJECT'>> {
-  project: Partial<bayes.maVoie.Project> & {projectId: string}
-}
-// TODO(émilie): Delete as it is no more used.
-function updateProject(project: UpdateProject['project']): UpdateProject {
-  return {project, type: 'UPDATE_PROJECT'}
-}
-
-interface UpdateStep extends Readonly<Action<'UPDATE_STEP'>> {
-  readonly projectId: string
-  readonly stepId: bayes.maVoie.StepId
-  readonly step: Partial<bayes.maVoie.ProjectStep>
-}
-// TODO(émilie): Delete as it is no more used.
-function updateStep(
-  projectId: string, stepId: bayes.maVoie.StepId, step: UpdateStep['step']): UpdateStep {
-  return {projectId, step, stepId, type: 'UPDATE_STEP'}
-}
 
 type Logout = Readonly<Action<'LOGOUT'>>
 const logoutAction: Logout = {type: 'LOGOUT'}
@@ -59,4 +24,4 @@ export interface RootState {
   user: bayes.maVoie.User
 }
 
-export {clearDataAction, createProjectAction, logoutAction, updateProject, updateStep, updateUser}
+export {clearDataAction, logoutAction}
