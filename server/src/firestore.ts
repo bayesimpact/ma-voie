f4f27176184c2f9a0813d94309f542fadc7637df
Cyrille Corpet <cyrille@bayesimpact.org>
2020-09-07 15:33:14 +0200

    [Server] Update at most one document. (#331)

--- server/src/firestore.ts
+++ server/src/firestore.ts
@@ -19,25 +19,25 @@ const updateField = (field: 'registeredAt' | 'validatedAt') => (
   const db = admin.firestore()
   return db.collectionGroup('partners').
     where('userPartnerId', '==', userPartnerId).
-    // TODO(cyrille): Only fetch one element.
-    select().get().then(snapshot => {
-      // TODO(cyrille): Return a 404 if snapshot is empty.
-      const batch = db.batch()
-      snapshot.forEach(doc => {
-        const oldSteps: readonly PartnerStep[] = doc.get('steps') || []
-        const newStep: PartnerStep = {[field]: new Date().toISOString(), projectId, stepId}
-        let alreadyHasStep = false
-        const updatedSteps = oldSteps.map(step => {
-          if (step.stepId === stepId && step.projectId === projectId) {
-            alreadyHasStep = true
-            // Only add `field` if it wasn't already there.
-            return {...newStep, ...step}
-          }
-          return step
-        })
-        batch.update(doc.ref, {steps: alreadyHasStep ? updatedSteps : [...oldSteps, newStep]})
+    limit(1).get().then(({docs: [partnerStepDoc]}) => {
+      if (!partnerStepDoc) {
+        // TODO(cyrille): Return a 404.
+        return
+      }
+      const oldSteps: readonly PartnerStep[] = partnerStepDoc.get('steps') || []
+      const newStep: PartnerStep = {[field]: new Date().toISOString(), projectId, stepId}
+      let alreadyHasStep = false
+      const updatedSteps = oldSteps.map(step => {
+        if (step.stepId === stepId && step.projectId === projectId) {
+          alreadyHasStep = true
+          // Only add `field` if it wasn't already there.
+          return {...newStep, ...step}
+        }
+        return step
       })
-      batch.commit()
+      return partnerStepDoc.ref.update({
+        steps: alreadyHasStep ? updatedSteps : [...oldSteps, newStep],
+      })
     }).catch(error => {
       functions.logger.error('An error occured while updating ' + userPartnerId, error)
     })
