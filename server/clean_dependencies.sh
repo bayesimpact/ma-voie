b0aa6c6234ed7ca2a4c38231b15a27b309195cbc
Cyrille <cyrille@bayesimpact.org>
2020-09-09 13:50:19 +0200

    [Server] Make sure the dependency list is relevant.

---
+++ server/clean_dependencies.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+npm run -s build
+readonly REQUIRED_LIBS=$(grep -hor 'require.*' lib/ |
+    sed -e 's/require("//' -e 's/");//' |
+    grep -v '^\.' |
+    sort | uniq)
+readonly DEPENDENCIES=$(sed -e '1,/dependencies/d' \
+    -e '/}/,$d' \
+    -e 's/^    "//' \
+    -e 's/":.*//' package.json)
+readonly MISSING_DEPENDENCIES=$(tr ' ' '\n' <<< "$REQUIRED_LIBS $DEPENDENCIES $DEPENDENCIES" |
+    sort | uniq -u)
+readonly UNUSED_DEPENDENCIES=$(tr ' ' '\n' <<< "$REQUIRED_LIBS $REQUIRED_LIBS $DEPENDENCIES" |
+    sort | uniq -u)
+if [ -z "$MISSING_DEPENDENCIES" ] && [ -z "$UNUSED_DEPENDENCIES" ]; then
+    echo "All dependencies are at their relevant position."
+    exit
+fi
+if [ "$MISSING_DEPENDENCIES" ]; then
+    echo 'The following dependencies should be added: '$MISSING_DEPENDENCIES
+fi
+if [ "$UNUSED_DEPENDENCIES" ]; then
+    echo 'The following dependencies should be removed or moved to devDependencies: '$UNUSED_DEPENDENCIES
+fi
+exit 1
+
