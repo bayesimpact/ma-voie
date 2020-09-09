c51d56abb598a8d206bfd92c9689ee247f17a2bd
Cyrille Corpet <cyrille@bayesimpact.org>
2020-09-09 10:56:30 +0200

    [Clean Code] Use async/await rather than Promise chaining. (#343)

--- server/src/count.ts
+++ server/src/count.ts
@@ -4,14 +4,23 @@ import {incrementPartnerCount, recomputePartnerCounts} from './firestore'
 
 // TODO(cyrille): Add tests.
 export const updateCount = functions.firestore.document('users/{userId}/partners/{partnerId}').
-  onCreate(snapshot => {
+  onCreate(async snapshot => {
     const partnerId = snapshot.get('partnerId')
     if (!partnerId) {
       return
     }
-    incrementPartnerCount(partnerId)
+    try {
+      await incrementPartnerCount(partnerId)
+    } catch (error) {
+      functions.logger.error('Unable to update count of partner users', error)
+    }
   })
 
 const countSchedule = functions.config().count?.schedule || '5 0 * * *'
-export const resetCount = functions.pubsub.schedule(countSchedule).
-  onRun(recomputePartnerCounts)
+export const resetCount = functions.pubsub.schedule(countSchedule).onRun(async () => {
+  try {
+    await recomputePartnerCounts()
+  } catch (error) {
+    functions.logger.error('Unable to recompute partner users counts', error)
+  }
+})
