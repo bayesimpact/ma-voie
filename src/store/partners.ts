f56c2a2adc6821a3d9a318ac44fdeccdd2d3c849
Cyrille Corpet <cyrille@bayesimpact.org>
2020-09-10 17:00:18 +0200

    [Demo] Allow demo-specific partner URLs. (#372)

--- src/store/partners.ts
+++ src/store/partners.ts
@@ -14,13 +14,17 @@ export interface Props {
   userCount?: number
 }
 
+const overridePartnerUrls = JSON.parse(config.partnerUrls)
+
 const PartnersWithAssets: readonly Readonly<Props>[] = Partners.
-  map(({logo, steps, ...partner}) => ({
+  map(({logo, partnerId, steps, url, ...partner}) => ({
     ...partner,
     // Add logos in the /assets/logo folder when adding new ones.
     logo: `${config.canonicalUrl}/assets/logo/${logo}`,
+    partnerId,
     // TODO(pascal): Try to avoid this type cast.
     steps: steps as readonly bayes.maVoie.StepId[],
+    url: overridePartnerUrls[partnerId] || url,
   }))
 
 export default PartnersWithAssets
