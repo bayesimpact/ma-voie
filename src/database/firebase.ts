caf81bd969e67e01ba4976d89674434abb890e52
Cyrille <cyrille@bayesimpact.org>
2020-09-10 10:14:41 +0200

    [Testing] Ensure the config is OK in demo.

--- src/database/firebase.ts
+++ src/database/firebase.ts
@@ -3,10 +3,12 @@ import firebase from 'firebase/app'
 require('firebase/auth')
 require('firebase/firestore')
 
-export const FirebaseApp = firebase.initializeApp(config.firebase)
+export const FirebaseApp = firebase.initializeApp(JSON.parse(config.firebase))
 export const Firestore = firebase.firestore()
 export const FirebaseAuth = FirebaseApp.auth()
 
+// eslint-disable-next-line no-console
+console.log(JSON.parse(config.firebase))
 export interface FirebaseErrorProps {
   code: string
   message: string
