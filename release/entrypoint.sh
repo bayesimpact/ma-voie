88e1ddb1d518f169d9a8c7276d4bcc061219c43e
Pascal Corpet <pcorpet@users.noreply.github.com>
2020-09-25 09:56:41 +0200

    [Amplitude] Switch to Amplitude projects owned by Ma Voie org. (#468)
    Fix #453

--- release/entrypoint.sh
+++ release/entrypoint.sh
@@ -14,10 +14,13 @@ function replace_string() {
 replace_string 'environment:"production"' 'environment:"demo"' "$JS_APP_FILES"
 
 # TODO(pascal): Read from const.json5 and const_dist.json5
-replace_string '6d5d1f5cb15e3cc946cbe3783e5f9e7f' '174aeb9469460975d8d6a14b31b6f916' "$JS_APP_FILES"
+# Amplitude Token
+replace_string '039e64319a02c90b21915451677416cd' 'ae2cd7f66aeafd893e37d421843b47b6' "$JS_APP_FILES"
+# Firebase config
 replace_string '{"apiKey":"AIzaSyAGH7JHYblSrKJsBSdr5UG0ZEBNdj8QRBM","appId":"1:740445000971:web:f73319eae6caf130c5be0e","authDomain":"ma-voie.firebaseapp.com","databaseURL":"https://ma-voie.firebaseio.com","messagingSenderId":"740445000971","projectId":"ma-voie","storageBucket":"ma-voie.appspot.com"}' \
     '{"apiKey":"AIzaSyCPdmI7WHWy6McHgu53OdW_QToZCAxMuT8","authDomain":"ma-voie-demo.firebaseapp.com","databaseURL":"https://ma-voie-demo.firebaseio.com","projectId":"ma-voie-demo","storageBucket":"ma-voie-demo.appspot.com","messagingSenderId":"1010721781393","appId":"1:1010721781393:web:a125d44474595b3ad67130"}' \
     "$JS_APP_FILES"
+# Canonical URL for Bob.
 replace_string 'https://www.bob-emploi.fr/intro' 'https://branch-master.bob-demo.bayes.org/intro' "$JS_APP_FILES"
 
 # TODO(pascal): Try to find a way to run the command if any.
