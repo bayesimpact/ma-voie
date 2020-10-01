b2f24a8e0797a4eda034e3a1a002e93202923a11
Sil Endale Ahanda <marielaure@bayesimpact.org>
2020-10-01 11:51:03 +0200

    [Account] Fix error message for invalid email address. For #505

--- src/components/pages/account.tsx
+++ src/components/pages/account.tsx
@@ -1,21 +1,19 @@
 import React, {useCallback, useEffect, useState} from 'react'
-import {Trans, useTranslation} from 'react-i18next'
+import {useTranslation} from 'react-i18next'
 import {useSelector} from 'react-redux'
-import {useFirebase, useFirestore} from 'react-redux-firebase'
 import {useHistory} from 'react-router'
 
+import {FirebaseAuth, FirebaseErrorProp} from 'database/firebase'
 import {useFastForward} from 'hooks/fast_forward'
-import {RootState} from 'store/actions'
-import {useUserId} from 'store/selections'
+import {RootState, updateUser, useDispatch} from 'store/actions'
 import {getPath} from 'store/url'
-import {validateEmail} from 'store/validations'
 
 import Button from 'components/button'
 import Input from 'components/input'
-import LayoutSignIn from 'components/layout_sign_in'
-import PasswordInput from 'components/password_input'
+import Layout from 'components/layout'
 
 const getUniqueExampleEmail = (): string => `test-${new Date().getTime()}@example.com`
+
 
 const inputStyle: React.CSSProperties = {
   border: `1px solid ${colors.SILVER_THREE}`,
@@ -33,61 +31,34 @@ const errorMessageStyle: React.CSSProperties = {
   fontSize: 13,
   marginLeft: 25,
 }
-const layoutStyle: React.CSSProperties = {
-  marginBottom: 20,
-}
-const checkboxLabelStyle: React.CSSProperties = {
-  alignItems: 'center',
-  cursor: 'pointer',
-  display: 'inline-flex',
-  marginTop: 10,
-}
-
 // This is a top level page and should never be nested in another one.
 // TOP LEVEL PAGE
 const AccountPage = (): React.ReactElement => {
   const {t} = useTranslation()
   const history = useHistory()
-  const firebase = useFirebase()
-  const firestore = useFirestore()
 
-  const userId = useUserId()
-  const {email, lastName, name, phone, jobSeeker, retraining} = useSelector(
-    ({firebase: {profile}}: RootState) => profile)
+  const name = useSelector(({user: {name}}: RootState) => name)
   const [inputName, setName] = useState(name || '')
   useEffect((): void => {
     name && setName(name)
   }, [name])
 
+  const lastName = useSelector(({user: {lastName}}: RootState) => lastName)
   const [inputLastName, setLastName] = useState(lastName || '')
   useEffect((): void => {
     lastName && setLastName(lastName)
   }, [lastName])
 
+  const email = useSelector(({user: {email}}: RootState) => email)
   const [inputEmail, setEmail] = useState(email || '')
   useEffect((): void => {
     email && setEmail(email)
   }, [email])
 
-  const [inputPhone, setPhone] = useState(phone || '')
-  useEffect((): void => {
-    phone && setPhone(phone)
-  }, [phone])
-
-  const [inputJobSeeker, setJobSeeker] = useState(jobSeeker || false)
-  const handleJobSeeker = useCallback((e): void => {
-    setJobSeeker(e.target.checked)
-  }, [setJobSeeker])
-
-  const [inputRetraining, setRetraining] = useState(retraining || false)
-  const handleRetraining = useCallback((e): void => {
-    setRetraining(e.target.checked)
-  }, [setRetraining])
-
   const [password, setPassword] = useState('')
   const [isErrorDisplayed, setIsErrorDisplayed] = useState(false)
   const [areErrorFields, setAreErrorFields] =
-    useState<{[K in 'email'|'lastName'|'name'|'phone'|'password']?: boolean}>({})
+    useState<{[K in 'email'|'lastName'|'name'|'password']?: boolean}>({})
   const [errorMessage, setErrorMessage] = useState('')
 
   const [updated, setUpdated] = useState(false)
@@ -102,48 +73,51 @@ const AccountPage = (): React.ReactElement => {
     }
   })
 
+  const dispatch = useDispatch()
   const onSave = useCallback((): void => {
-    const isEmailValid = validateEmail(inputEmail)
     const errorsFields = {
-      email: !inputEmail || !isEmailValid,
+      // TODO(émilie): validate the email
+      email: !inputEmail,
       lastName: !inputLastName,
       name: !inputName,
-      password: !password && !userId,
-      phone: !inputPhone,
+      password: !password,
     }
     setAreErrorFields(errorsFields)
-    if (Object.values(errorsFields).some(field => field)) {
+    if (!inputName || !inputLastName || !inputEmail || !password) {
+      setIsErrorDisplayed(true)
       return
     }
     setIsErrorDisplayed(false)
     const update = {
       ...name === inputName ? {} : {name: inputName},
       ...lastName === inputLastName ? {} : {lastName: inputLastName},
-      ...email === inputEmail || userId ? {} : {email: inputEmail},
-      ...phone === inputPhone ? {} : {phone: inputPhone},
-      ...inputJobSeeker ? {jobSeeker: true} : {jobSeeker: false},
-      ...inputRetraining ? {retraining: true} : {retraining: false},
+      ...email === inputEmail ? {} : {email: inputEmail},
     }
     if (Object.keys(update).length) {
+      dispatch(updateUser(update))
       // TODO(émilie): Move to actions.ts
-      if (!userId) {
-        firebase.createUser({email: inputEmail, password}, update).
-          then(
-            () => setUpdated(true),
-            (error) => {
-              setErrorMessage(error.message)
-              return
-            },
-          )
-      } else {
-        firestore.update({collection: 'users', doc: userId}, update)
-        setUpdated(true)
-      }
+      FirebaseAuth.createUserWithEmailAndPassword(inputEmail, password).
+        catch((error: FirebaseErrorProp) => {
+          // TODO(émilie): delete login here and handle login somewhere else
+          if (error.code === 'auth/email-already-in-use') {
+            FirebaseAuth.signInWithEmailAndPassword(inputEmail, password).
+              catch((error: FirebaseErrorProp) => {
+                setErrorMessage(error.message)
+                return
+              })
+          } else {
+            setErrorMessage(error.message)
+            return
+          }
+        })
+      // TODO(émilie): After auth, save the user ID in the user
+      setUpdated(true)
     }
-  }, [email, firebase, firestore, name, inputEmail, inputJobSeeker, inputName, lastName,
-    inputLastName, inputPhone, inputRetraining, password, phone, setErrorMessage, userId])
+  }, [dispatch,
+    email, name, inputEmail, inputName, lastName, inputLastName,
+    password, setErrorMessage])
   useFastForward(() => {
-    if (inputName && inputLastName && inputEmail && inputPhone && password) {
+    if (inputName && inputLastName && inputEmail && password) {
       onSave()
       return
     }
@@ -156,9 +130,6 @@ const AccountPage = (): React.ReactElement => {
     if (!inputEmail) {
       setEmail(getUniqueExampleEmail())
     }
-    if (!inputPhone) {
-      setPhone('0123456789')
-    }
     if (!password) {
       setPassword('password')
     }
@@ -166,8 +137,7 @@ const AccountPage = (): React.ReactElement => {
 
   const buttonStyle: React.CSSProperties = {
     marginTop: 20,
-    opacity: !inputName || !inputLastName || !inputEmail ||
-      !inputPhone || (!password && !userId) ? 0.75 : 1,
+    opacity: !inputName || !inputLastName || !inputEmail || !password ? 0.75 : 1,
   }
 
   const errorValidationStyle: React.CSSProperties = {
@@ -186,72 +156,51 @@ const AccountPage = (): React.ReactElement => {
     ...inputStyle,
     borderColor: areErrorFields.email ? colors.RED_ERROR : colors.SILVER_THREE,
   }
-  const inputPhoneStyle: React.CSSProperties = {
-    ...inputStyle,
-    borderColor: areErrorFields.phone ? colors.RED_ERROR : colors.SILVER_THREE,
-  }
   const inputPasswordStyle: React.CSSProperties = {
     ...inputStyle,
     borderColor: areErrorFields.password ? colors.RED_ERROR : colors.SILVER_THREE,
   }
 
-  // TODO(pascal): Investigate how much the layoutStyle is useful.
-  return <LayoutSignIn bigTitle={t('Inscription')} style={layoutStyle}>
+  return <Layout bigTitle={t('Inscription')}>
     <Input
       placeholder={t('Prénom')} style={inputNameStyle}
       autoComplete="given-name"
       value={inputName} onChange={setName} />
-    {areErrorFields.name ?
-      <div style={errorMessageStyle}><sup>*</sup>{t('Champ obligatoire')}</div> :
-      null}
+    {areErrorFields.name ? <div style={errorMessageStyle}>
+      {t('<sup>*</sup>Champ obligatoire')}
+    </div> : null}
     <Input
       placeholder={t('Nom')} style={inputLastNameStyle}
       autoComplete="family-name"
       value={inputLastName} onChange={setLastName} />
-    {areErrorFields.lastName ?
-      <div style={errorMessageStyle}><sup>*</sup>{t('Champ obligatoire')}</div> :
-      null}
+    {areErrorFields.lastName ? <div style={errorMessageStyle}>
+      {t('<sup>*</sup>Champ obligatoire')}
+    </div> : null}
     <Input
       placeholder={t('Email')} style={inputEmailStyle}
-      autoComplete="email" disabled={userId ? true : false}
+      autoComplete="email"
       value={inputEmail} onChange={setEmail} />
-    {areErrorFields.email ?
-      <div style={errorMessageStyle}><sup>*</sup>{t('Champ obligatoire')}</div> :
-      null}
+    {areErrorFields.email ? <div style={errorMessageStyle}>
+      {t('<sup>*</sup>Adresse email incorrecte')}
+    </div> : null}
     <Input
-      placeholder={t('Numéro de téléphone')} style={inputPhoneStyle}
-      autoComplete="phone"
-      value={inputPhone} onChange={setPhone} type="tel" />
-    {areErrorFields.phone ?
-      <div style={errorMessageStyle}><sup>*</sup>{t('Champ obligatoire')}</div> :
-      null}
-    {userId ? null : <PasswordInput
-      style={inputPasswordStyle} autoComplete="new-password"
-      value={password} onChange={setPassword} />}
-    {!userId && areErrorFields.password ?
-      <div style={errorMessageStyle}>
-        <sup>*</sup>{t('Champ obligatoire, vérifiez votre email')}
-      </div> :
-      null}
-    <label style={checkboxLabelStyle}>
-      <input defaultChecked={inputJobSeeker} onChange={handleJobSeeker} type="checkbox" />
-      <Trans>Je suis en recherche d'emploi</Trans>
-    </label>
-    <label style={checkboxLabelStyle}>
-      <input defaultChecked={inputRetraining} onChange={handleRetraining} type="checkbox" />
-      <Trans>Je suis en reconversion professionnelle</Trans>
-    </label>
+      placeholder={t('Mot de passe')} style={inputPasswordStyle}
+      type="password" autoComplete="new-password"
+      value={password} onChange={setPassword} />
+    {areErrorFields.password ? <div style={errorMessageStyle}>
+      {t('<sup>*</sup>Champ obligatoire')}
+    </div> : null}
     <Button type="secondLevel" onClick={onSave} style={buttonStyle} >
       {t('Valider')}
     </Button>
-    {updated ? t('Vos informations ont été mises à jour.') : null}
+    {updated ? t('Vos identifiants ont été mis à jour.') : null}
     {isErrorDisplayed ? <div style={errorValidationStyle}>
       {t('Veuillez saisir les champs obligatoires pour continuer')}
     </div> : null}
     {errorMessage ? <div style={errorValidationStyle}>
       {errorMessage}
     </div> : null}
-  </LayoutSignIn>
+  </Layout>
 }
 
 export default React.memo(AccountPage)
