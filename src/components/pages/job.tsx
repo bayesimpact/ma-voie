c9bc819933223dddd53eea8e98c81c9aab79b47a
Émilie Guth <emilie@bayesimpact.org>
2020-10-20 11:27:36 +0200

    [Easy Dev] Correct bug of invisible text in input in Safari PATH=/definition/metier
    Fixed #547

--- src/components/pages/job.tsx
+++ src/components/pages/job.tsx
@@ -23,7 +23,7 @@ const inputStyle: React.CSSProperties = {
   fontFamily: 'ProximaSoft',
   fontSize: 16,
   height: 24,
-  lineHeight: 24,
+  lineHeight: '24px',
   padding: '18px 25px',
   width: 'calc(100% - 50px)',
 }
