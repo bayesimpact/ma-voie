0ea1bbf1393d3a3045df203d9bf3bbfda9cc19d6
Émilie Guth <emilie@bayesimpact.org>
2020-11-09 17:09:56 +0100

    [Statistics] Added more data for the statistics

--- analytics/export_users.ts
+++ analytics/export_users.ts
@@ -10,18 +10,28 @@ import {promises as fs} from 'fs'
 import serviceAccount from './serviceAccountKey.json'
 
 type User = bayes.maVoie.User
+type Partner = bayes.maVoie.PartnerIdentification
+
+const steps = ['definition', 'skills', 'training', 'interview'] as readonly bayes.maVoie.StepId[]
 
 // List of fields to export with the function to compute the value for each user.
 const fields = {
   // Whether the user has a clear job or not.
-  hasNoClearJob: (user: unknown, data: User): string =>
-    !!data.projects?.[0]?.hasNoClearJob + '',
+  hasNoClearJob: (user: unknown, data: User): string => !!data.projects?.[0]?.hasNoClearJob + '',
+  jobName: (user: unknown, data: User): string => data.projects?.[0]?.job?.jobGroup?.name || '',
   // Their objective.
   objective: (user: unknown, data: User): string => data.projects?.[0]?.objective || '',
   // The time of registration of the user.
   registerTime: (user: admin.firestore.QueryDocumentSnapshot): string =>
     user.createTime.toDate().toISOString(),
-} as const
+  partners: (user: unknown, data: User, partners?: Partner[]): string => {
+    return partners?.map(partner => (partner?.steps ? partner.steps[0].stepId + ',' + partner?.partnerId : '')).filter(partner => partner.length > 0).join(',') || ''
+  },
+  // Steps data
+  ...Object.fromEntries(steps.map((step: bayes.maVoie.StepId) => [`${step}Partner`, (user: unknown, data: User): string => data.projects?.[0].steps?.[step]?.selectedPartnerId || ''])),
+  ...Object.fromEntries(steps.map((step: bayes.maVoie.StepId) => [`${step}Completed`, (user: unknown, data: User): string => data.projects?.[0].steps?.[step]?.completed || ''])),
+}
+
 const fieldNames = Object.keys(fields) as readonly (keyof typeof fields)[]
 
 const prepareCsvLine = (row: readonly string[]): string =>
@@ -37,7 +47,9 @@ const exportUsers = async (): Promise<void> => {
   await outputFile.write(prepareCsvLine(fieldNames))
   await Promise.all(users.docs.map(async user => {
     const data = user.data() as User
-    await outputFile.write(prepareCsvLine(fieldNames.map(name => fields[name](user, data))))
+    const userPartners = await db.collection('users/' + data.userId + '/partners').get()
+    const partners = userPartners.docs.map(doc => doc.data() as Partner)
+    await outputFile.write(prepareCsvLine(fieldNames.map(name => fields[name](user, data, partners))))
   }))
   outputFile.close()
 }
