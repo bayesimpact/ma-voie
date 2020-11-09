fd4b5a4c190b99708318682e08f47c162991a763
Émilie Guth <emilie@bayesimpact.org>
2020-11-09 14:56:02 +0100

    [Statistics] Added more data for the statistics

--- analytics/export_users.ts
+++ analytics/export_users.ts
@@ -10,17 +10,31 @@ import {promises as fs} from 'fs'
 import serviceAccount from './serviceAccountKey.json'
 
 type User = bayes.maVoie.User
+type Partner = bayes.maVoie.PartnerIdentification
 
 // List of fields to export with the function to compute the value for each user.
 const fields = {
   // Whether the user has a clear job or not.
-  hasNoClearJob: (user: unknown, data: User): string =>
+  hasNoClearJob: (user: unknown, data: User, partners?: Partner[]): string =>
     !!data.projects?.[0]?.hasNoClearJob + '',
   // Their objective.
-  objective: (user: unknown, data: User): string => data.projects?.[0]?.objective || '',
+  objective: (user: unknown, data: User, partners?: Partner[]): string => data.projects?.[0]?.objective || '',
+  jobName: (user: unknown, data: User, partners?: Partner[]): string => data.projects?.[0]?.job?.jobGroup?.name || '',
+  // Steps data
+  definitionPartner: (user: unknown, data: User, partners?: Partner[]): string => data.projects?.[0].steps?.definition?.selectedPartnerId || '',
+  definitionCompleted: (user: unknown, data: User, partners?: Partner[]): string => data.projects?.[0].steps?.definition?.completed || '',
+  skillsPartner: (user: unknown, data: User, partners?: Partner[]): string => data.projects?.[0].steps?.skills?.selectedPartnerId || '',
+  skillsCompleted: (user: unknown, data: User, partners?: Partner[]): string => data.projects?.[0].steps?.skills?.completed || '',
+  skills: (user: unknown, data: User): string => data.projects?.[0].skills?.length.toString() || '0',
+  trainingPartner: (user: unknown, data: User, partners?: Partner[]): string => data.projects?.[0].steps?.training?.selectedPartnerId || '',
+  trainingCompleted: (user: unknown, data: User, partners?: Partner[]): string => data.projects?.[0].steps?.training?.completed || '',
+  interviewPartner: (user: unknown, data: User, partners?: Partner[]): string => data.projects?.[0].steps?.interview?.selectedPartnerId || '',
   // The time of registration of the user.
   registerTime: (user: admin.firestore.QueryDocumentSnapshot): string =>
     user.createTime.toDate().toISOString(),
+  partners: (user: unknown, data: User, partners?: Partner[]): string => {
+    return partners?.map(partner => (partner?.steps ? partner.steps[0].stepId + ',' + partner?.partnerId : '')).filter(partner => partner.length > 0).join(',') || ''
+  }
 } as const
 const fieldNames = Object.keys(fields) as readonly (keyof typeof fields)[]
 
@@ -37,7 +51,9 @@ const exportUsers = async (): Promise<void> => {
   await outputFile.write(prepareCsvLine(fieldNames))
   await Promise.all(users.docs.map(async user => {
     const data = user.data() as User
-    await outputFile.write(prepareCsvLine(fieldNames.map(name => fields[name](user, data))))
+    const userPartners = await db.collection('users/' + data.userId + '/partners').get()
+    const partners = userPartners.docs.map(doc => doc.data() as Partner)
+    await outputFile.write(prepareCsvLine(fieldNames.map(name => fields[name](user, data, partners))))
   }))
   outputFile.close()
 }
