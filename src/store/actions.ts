2d40af5944593f9e5ac0b5581bfb5c2a749d8999
Émilie Guth <emilie@bayesimpact.org>
2020-12-07 16:39:53 +0100

    [Clean Code] Delete cast as the issue is corrected

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -5,7 +5,7 @@ import {getI18n} from 'react-i18next'
 import {useDispatch as genericUseDispatch} from 'react-redux'
 import {Action, Dispatch} from 'redux'
 import {ThunkAction, ThunkDispatch} from 'redux-thunk'
-import {Credentials, ExtendedFirestoreInstance, FirebaseReducer, FirestoreReducer, getFirebase}
+import {Credentials, FirebaseReducer, FirestoreReducer, getFirebase}
   from 'react-redux-firebase'
 import {actionTypes} from 'redux-firestore'
 import sha1 from 'sha1'
@@ -168,9 +168,7 @@ ThunkAction<Promise<string>, RootState, unknown, LinkPartner> {
 const calendlyClickAction:
 ThunkAction<Promise<void>, RootState, unknown, Readonly<Action<'REQUEST_CALENDLY_CONTACT'>>> =
   async (dispatch): Promise<void> => {
-    // TODO(émilie): delete cast
-    // see https://github.com/prescottprue/react-redux-firebase/issues/1012
-    const firestore = getFirebase().firestore as unknown as ExtendedFirestoreInstance
+    const firestore = getFirebase().firestore
     await getFirebase().firestore().
       doc('analytics/calendly').
       set({value: firestore.FieldValue.increment(1)}, {merge: true}).
