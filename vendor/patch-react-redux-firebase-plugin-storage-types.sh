8a6b496d57353a1f71b32ff09867d35243fc3fa2
Émilie Guth <emilie@bayesimpact.org>
2021-01-06 15:29:57 +0100

    [Clean code] Bug fixed in react-redux-firebase 3.9.0 (#588)

--- vendor/patch-react-redux-firebase-plugin-storage-types.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# TODO(émilie): Correct when https://github.com/prescottprue/react-redux-firebase/issues/997 is corrected.
-sed -i -e "s/StorageTypes\.UploadTaskSnapshot\.metadata/StorageTypes\.UploadTaskSnapshot['metadata']/" node_modules/react-redux-firebase/index.d.ts
