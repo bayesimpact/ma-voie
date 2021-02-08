e393345ef569c486b2058fab99a78f152e0e98c5
Émilie Guth <emilie@bayesimpact.org>
2021-02-08 14:37:39 +0100

    [Clean code] Updates master into main branch

--- release/tag.sh
+++ release/tag.sh
@@ -5,7 +5,7 @@ set -e
 # e.g. 2019-02-12_01.
 readonly TAG_PREFIX="$(date +%Y-%m-%d)_"
 readonly TAG="${TAG_PREFIX}$(printf %02d $(git tag -l "${TAG_PREFIX}*" | wc -l))"
-readonly BRANCH=${2:-master}
+readonly BRANCH=${2:-main}
 
 git fetch origin $BRANCH
 git tag "${TAG}" origin/$BRANCH
