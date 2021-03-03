a4f5733f226379ca68484715a9ab0ad12016bf9f
Émilie Guth <emilie@bayesimpact.org>
2021-03-03 18:08:37 +0100

    [Marketing] Add GTM in the site

--- src/index.tsx
+++ src/index.tsx
@@ -64,8 +64,21 @@ export const Template = (props: TemplateProps): React.ReactElement => {
       <meta property="og:url" content={config.canonicalUrl} />
 
       <link rel="icon" href={faviconImage} type="image/x-icon" />
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-N9SWSWB');</script>
+      <!-- End Google Tag Manager -->
     </head>
     <body style={{margin: 0}}>
+      <!-- Google Tag Manager (noscript) -->
+      <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N9SWSWB"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>
+      </noscript>
+      <!-- End Google Tag Manager (noscript) -->
       <div id="app">{/* TODO(cyrille): Add a static element here. */}</div>
     </body>
   </html>
