928aaa6c3ff5d3c0429c4b245e1e5eab07ff7ae4
El Mehdi Boumhicha <96.elmehdi@gmail.com>
2021-09-15 15:43:38 +0200

    small code to test release branch (#676)

--- test/webpack/colors_test.ts
+++ test/webpack/colors_test.ts
@@ -4,6 +4,6 @@ import {colorToAlpha} from 'components/colors'
 
 describe('colorToAlpha', () => {
   it('should add an alpha to an RGB color', () => {
-    expect(colorToAlpha('#ffffff', .1)).to.eq('rgba(255, 255, 255, 0.1)')
+    expect(colorToAlpha('#ffffff', .2)).to.eq('rgba(255, 255, 255, 0.2)')
   })
 })
