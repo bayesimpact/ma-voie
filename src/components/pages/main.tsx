441248655ec2a272a7d2ad3f081d55d4504a6f5e
mehdi <mehdi@dev.flc>
2021-11-23 15:49:41 +0100

    comment to solve linter issues

--- src/components/pages/main.tsx
+++ src/components/pages/main.tsx
@@ -54,166 +54,166 @@ const MaintenanceApp = (): React.ReactElement => (
   </div>
 )
 
-const App = (): React.ReactElement => {
-  const {hash, pathname, search} = useLocation()
-  const isConnected = useUserId() !== undefined
-  const defineAndGetPath = useSubPathDefiner()
-  useEffect((): void => logPage(pathname), [pathname])
-  useEffect((): void => {
-    const urlParams = new URLSearchParams(window.location.search)
-    const utmSource = urlParams.get('utm_source')
-    if (!utmSource) {
-      return
-    }
-    localStorage.setItem('utm_source', utmSource)
-  }, [])
-  // i18next-extract-mark-ns-start url
-  return <Switch>
-    {isConnected ? <Route
-      path={`/:step${defineAndGetPath('PARTNERS_INTERNAL')}`} component={PartnersPage} /> : null}
-    {isConnected ? <Route
-      path={`/:step${defineAndGetPath('PARTNERS_EXTERNAL')}`} component={PartnersPage} /> : null}
-    {isConnected ? <Route path={`/:step${defineAndGetPath('JOB')}`} component={JobPage} /> : null}
-    <Route path={defineAndGetPath('ACCOUNT')} component={AccountPage} />
-    <Route path={defineAndGetPath('CONGRATULATIONS')} component={CongratulationsPage} />
-    {isConnected ? null : <Route path={defineAndGetPath('LOGIN')} component={LoginPage} />}
-    {isConnected ?
-      <Route path={defineAndGetPath('DEFINITION')} component={DefinitionPage} /> : null}
-    {isConnected ? <Route path={defineAndGetPath('SKILLS')} component={SkillsPage} /> : null}
-    {isConnected ? <Route path={defineAndGetPath('TRAINING')} component={TrainingPage} /> : null}
-    {isConnected ? <Route path={defineAndGetPath('INTERVIEW')}>
-      <Redirect
-        to={`${defineAndGetPath('INTERVIEW')}${defineAndGetPath('PARTNERS_INTERNAL')}`} />
-    </Route> : null}
-    <Route path={defineAndGetPath('FORGOT_PASSWORD')} component={ForgotPasswordPage} />
-    <Route path={defineAndGetPath('MENU')} component={MenuPage} />
-    <Route path={defineAndGetPath('MENU_SITE')} component={MenuSitePage} />
-    <Route path={defineAndGetPath('MISSION')} component={MissionPage} />
-    <Route path={defineAndGetPath('PASSWORD')} component={PasswordPage} />
-    {isConnected ? null : <Route path={defineAndGetPath('SIGNUP')} component={SignupPage} />}
-    <Route path={defineAndGetPath('SPLASH')} component={SplashPage} />
-    <Route path={defineAndGetPath('STEPS')} component={StepsPage} />
-    <Route path={defineAndGetPath('TEAM')} component={TeamPage} />
-    <Route path={defineAndGetPath('TERMS')} component={TermsPage} />
-    <Route path={defineAndGetPath('COMPONENTS')} component={ComponentsPage} />
-    <Redirect to={defineAndGetPath('SPLASH') + search + hash} />
-  </Switch>
-  // i18next-extract-mark-ns-stop url
-}
-const MemoApp = React.memo(App)
+// const App = (): React.ReactElement => {
+//   const {hash, pathname, search} = useLocation()
+//   const isConnected = useUserId() !== undefined
+//   const defineAndGetPath = useSubPathDefiner()
+//   useEffect((): void => logPage(pathname), [pathname])
+//   useEffect((): void => {
+//     const urlParams = new URLSearchParams(window.location.search)
+//     const utmSource = urlParams.get('utm_source')
+//     if (!utmSource) {
+//       return
+//     }
+//     localStorage.setItem('utm_source', utmSource)
+//   }, [])
+//   // i18next-extract-mark-ns-start url
+//   return <Switch>
+//     {isConnected ? <Route
+//       path={`/:step${defineAndGetPath('PARTNERS_INTERNAL')}`} component={PartnersPage} /> : null}
+//     {isConnected ? <Route
+//       path={`/:step${defineAndGetPath('PARTNERS_EXTERNAL')}`} component={PartnersPage} /> : null}
+//     {isConnected ? <Route path={`/:step${defineAndGetPath('JOB')}`} component={JobPage} /> : null}
+//     <Route path={defineAndGetPath('ACCOUNT')} component={AccountPage} />
+//     <Route path={defineAndGetPath('CONGRATULATIONS')} component={CongratulationsPage} />
+//     {isConnected ? null : <Route path={defineAndGetPath('LOGIN')} component={LoginPage} />}
+//     {isConnected ?
+//       <Route path={defineAndGetPath('DEFINITION')} component={DefinitionPage} /> : null}
+//     {isConnected ? <Route path={defineAndGetPath('SKILLS')} component={SkillsPage} /> : null}
+//     {isConnected ? <Route path={defineAndGetPath('TRAINING')} component={TrainingPage} /> : null}
+//     {isConnected ? <Route path={defineAndGetPath('INTERVIEW')}>
+//       <Redirect
+//         to={`${defineAndGetPath('INTERVIEW')}${defineAndGetPath('PARTNERS_INTERNAL')}`} />
+//     </Route> : null}
+//     <Route path={defineAndGetPath('FORGOT_PASSWORD')} component={ForgotPasswordPage} />
+//     <Route path={defineAndGetPath('MENU')} component={MenuPage} />
+//     <Route path={defineAndGetPath('MENU_SITE')} component={MenuSitePage} />
+//     <Route path={defineAndGetPath('MISSION')} component={MissionPage} />
+//     <Route path={defineAndGetPath('PASSWORD')} component={PasswordPage} />
+//     {isConnected ? null : <Route path={defineAndGetPath('SIGNUP')} component={SignupPage} />}
+//     <Route path={defineAndGetPath('SPLASH')} component={SplashPage} />
+//     <Route path={defineAndGetPath('STEPS')} component={StepsPage} />
+//     <Route path={defineAndGetPath('TEAM')} component={TeamPage} />
+//     <Route path={defineAndGetPath('TERMS')} component={TermsPage} />
+//     <Route path={defineAndGetPath('COMPONENTS')} component={ComponentsPage} />
+//     <Redirect to={defineAndGetPath('SPLASH') + search + hash} />
+//   </Switch>
+//   // i18next-extract-mark-ns-stop url
+// }
+// const MemoApp = React.memo(App)
 
-interface AppState {
-  history: History
-  store: Store<RootState, AllActions>
-}
+// interface AppState {
+//   history: History
+//   store: Store<RootState, AllActions>
+// }
 
-function createHistoryAndStore(): AppState {
-  const history = createBrowserHistory()
+// function createHistoryAndStore(): AppState {
+//   const history = createBrowserHistory()
 
-  const finalCreateStore = composeWithDevTools(applyMiddleware(
-    thunk,
-    localStorageMiddleware,
-    routerMiddleware(history),
-  ))(createStore)
+//   const finalCreateStore = composeWithDevTools(applyMiddleware(
+//     thunk,
+//     localStorageMiddleware,
+//     routerMiddleware(history),
+//   ))(createStore)
 
-  // Create the store that will be provided to connected components via Context.
-  const store = finalCreateStore<RootState, AllActions>(
-    combineReducers({
-      firebase: firebaseReducer as Reducer<FirebaseReducer.Reducer<bayes.maVoie.User>>,
-      firestore: firestoreReducer,
-      router: connectRouter(history),
-      user,
-    }),
-  )
-  if (module.hot) {
-    module.hot.accept(['store/app_reducer'], (): void => {
-      const {user: newUser} =
-        require('store/app_reducer')
-      store.replaceReducer(combineReducers({
-        firebase: firebaseReducer as Reducer<FirebaseReducer.Reducer<bayes.maVoie.User>>,
-        firestore: firestoreReducer,
-        router: connectRouter(history),
-        user: newUser as typeof user,
-      }))
-    })
-  }
-  return {history, store}
-}
-// The app that will be augmented by top level wrappers.
-const WrappedApp = (): React.ReactElement => {
-  const [{history, store}] = useState(createHistoryAndStore)
-  const rrfConfig = {
-    onAuthStateChanged: (user: User|null): void => {
-      if (user) {
-        return
-      }
-      store.dispatch({type: actionTypes.CLEAR_DATA as ClearDataType})
-    },
-    useFirestoreForProfile: true,
-    userProfile: 'users',
-  }
-  const rrfProps = {
-    config: rrfConfig,
-    createFirestoreInstance,
-    dispatch: store.dispatch,
-    firebase,
-  }
-  // @ts-ignore
-  window.dataLayer = {
-    config: 'G-SMDX3JJ535',
-    js: new Date(),
-  }
+//   // Create the store that will be provided to connected components via Context.
+//   const store = finalCreateStore<RootState, AllActions>(
+//     combineReducers({
+//       firebase: firebaseReducer as Reducer<FirebaseReducer.Reducer<bayes.maVoie.User>>,
+//       firestore: firestoreReducer,
+//       router: connectRouter(history),
+//       user,
+//     }),
+//   )
+//   if (module.hot) {
+//     module.hot.accept(['store/app_reducer'], (): void => {
+//       const {user: newUser} =
+//         require('store/app_reducer')
+//       store.replaceReducer(combineReducers({
+//         firebase: firebaseReducer as Reducer<FirebaseReducer.Reducer<bayes.maVoie.User>>,
+//         firestore: firestoreReducer,
+//         router: connectRouter(history),
+//         user: newUser as typeof user,
+//       }))
+//     })
+//   }
+//   return {history, store}
+// }
+// // The app that will be augmented by top level wrappers.
+// const WrappedApp = (): React.ReactElement => {
+//   const [{history, store}] = useState(createHistoryAndStore)
+//   const rrfConfig = {
+//     onAuthStateChanged: (user: User|null): void => {
+//       if (user) {
+//         return
+//       }
+//       store.dispatch({type: actionTypes.CLEAR_DATA as ClearDataType})
+//     },
+//     useFirestoreForProfile: true,
+//     userProfile: 'users',
+//   }
+//   const rrfProps = {
+//     config: rrfConfig,
+//     createFirestoreInstance,
+//     dispatch: store.dispatch,
+//     firebase,
+//   }
+//   // @ts-ignore
+//   window.dataLayer = {
+//     config: 'G-SMDX3JJ535',
+//     js: new Date(),
+//   }
 
-  // @ts-ignore
-  window.axeptioSettings = {
-    clientId: '6143060f7d0d6631a194f2d9',
-    cookiesVersion: 'custom_release_13/10',
-  }
+//   // @ts-ignore
+//   window.axeptioSettings = {
+//     clientId: '6143060f7d0d6631a194f2d9',
+//     cookiesVersion: 'custom_release_13/10',
+//   }
 
-  /* AxeptIO Config */
-  const axeptioJs = document.createElement('script')
-  axeptioJs.setAttribute('async', '')
-  axeptioJs.setAttribute('src', 'https://static.axept.io/sdk.js')
-  document.head.appendChild(axeptioJs)
+//   /* AxeptIO Config */
+//   const axeptioJs = document.createElement('script')
+//   axeptioJs.setAttribute('async', '')
+//   axeptioJs.setAttribute('src', 'https://static.axept.io/sdk.js')
+//   document.head.appendChild(axeptioJs)
 
-  // @ts-ignore
-  void 0 === window._axcb && (window._axcb = [])
-  // @ts-ignore
-  window._axcb.push(function(axeptio) {
-  // @ts-ignore
-    axeptio.on('cookies:complete', function(choices) {
-      if (choices.google_analytics) {
-        /* Google Tag Manager Config */
-        const tagManagerArgs = {gtmId: 'GTM-N9SWSWB'}
-        TagManager.initialize(tagManagerArgs)
-      }
-      if (choices.autopilot) {
-        /* Auto Pilot Config */
-        const autopilotJs = document.createElement('script')
-        autopilotJs.setAttribute('async', '')
-        autopilotJs.setAttribute('src', 'https://fastfinch.co/anywhere/275cb238a5ce440198d265b1931d6b113fe2b83923fa4bfcad2ffe19c01edfe1')
-        document.head.appendChild(autopilotJs)
-      }
-      if (choices.facebook_pixel) {
-        /* Facebook Pixel Config */
-        ReactPixel.init('164192945537808')
-        ReactPixel.pageView()
-      }
-    })
-  })
+//   // @ts-ignore
+//   void 0 === window._axcb && (window._axcb = [])
+//   // @ts-ignore
+//   window._axcb.push(function(axeptio) {
+//   // @ts-ignore
+//     axeptio.on('cookies:complete', function(choices) {
+//       if (choices.google_analytics) {
+//         /* Google Tag Manager Config */
+//         const tagManagerArgs = {gtmId: 'GTM-N9SWSWB'}
+//         TagManager.initialize(tagManagerArgs)
+//       }
+//       if (choices.autopilot) {
+//         /* Auto Pilot Config */
+//         const autopilotJs = document.createElement('script')
+//         autopilotJs.setAttribute('async', '')
+//         autopilotJs.setAttribute('src', 'https://fastfinch.co/anywhere/275cb238a5ce440198d265b1931d6b113fe2b83923fa4bfcad2ffe19c01edfe1')
+//         document.head.appendChild(autopilotJs)
+//       }
+//       if (choices.facebook_pixel) {
+//         /* Facebook Pixel Config */
+//         ReactPixel.init('164192945537808')
+//         ReactPixel.pageView()
+//       }
+//     })
+//   })
 
-  // TODO(pascal): Add a scroll-up on page change.
-  return <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <ReactReduxFirebaseProvider {...rrfProps}>
-        {/* TODO(cyrille): Add a nice waiting page. */}
-        <Suspense fallback={<div />}>
-          <MemoApp />
-        </Suspense>
-      </ReactReduxFirebaseProvider>
-    </ConnectedRouter>
-  </Provider>
-}
-const MemoWrappedApp = React.memo(WrappedApp)
+//   // TODO(pascal): Add a scroll-up on page change.
+//   return <Provider store={store}>
+//     <ConnectedRouter history={history}>
+//       <ReactReduxFirebaseProvider {...rrfProps}>
+//         {/* TODO(cyrille): Add a nice waiting page. */}
+//         <Suspense fallback={<div />}>
+//           <MemoApp />
+//         </Suspense>
+//       </ReactReduxFirebaseProvider>
+//     </ConnectedRouter>
+//   </Provider>
+// }
+// const MemoWrappedApp = React.memo(WrappedApp)
 
 export {MaintenanceApp as App}
